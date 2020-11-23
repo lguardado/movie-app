96bb66d2761ca4f1fd896b315b7f9e0a5ee4b569
lguardado <lucas.guardado@gmail.com>
2020-11-23 11:38:00 -0300

    Refactor Center component to receive additional props

--- src/components/Center/Center.js
+++ src/components/Center/Center.js
@@ -3,16 +3,20 @@ import { View } from 'react-native';
 import { PropTypes } from 'prop-types';
 import styles from './styles';
 
-const Center = ({ children }) => {
-  return <View style={styles.centeredContainer}>{children}</View>;
+const Center = ({ children, style }) => {
+  return (
+    <View style={{ ...styles.centeredContainer, ...style }}>{children}</View>
+  );
 };
 
 export default Center;
 
 Center.propTypes = {
   children: PropTypes.object,
+  style: PropTypes.object,
 };
 
 Center.defaultProps = {
   children: null,
+  style: {},
 };
