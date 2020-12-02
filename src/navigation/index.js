e8436803e8481b5097df800b5926c79fe7fb45b7
lguardado <lucas.guardado@gmail.com>
2020-12-02 14:44:09 -0300

    Remove unused imports

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,8 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import AuthNavigator from 'navigation/AuthNavigator';
 import AppNavigator from 'navigation/AppNavigator';
 
 function Navigation({ theme }) {
