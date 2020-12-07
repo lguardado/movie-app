306188e9ec0088abdfc22a3c5a8b792f3ddd4c95
lguardado <lucas.guardado@gmail.com>
2020-12-07 17:22:51 -0300

    Remove more unused code.

--- src/__mocks__/@react-native-community/netinfo.js
+++ src/__mocks__/@react-native-community/netinfo.js
@@ -24,18 +24,3 @@ RNCNetInfoMock.fetch.mockResolvedValue(defaultState);
 RNCNetInfoMock.useNetInfo.mockResolvedValue(defaultState);
 
 module.exports = RNCNetInfoMock;
-
-// export default {
-//   getCurrentConnectivity: jest.fn(),
-//   isConnectionMetered: jest.fn(),
-//   addListener: jest.fn(),
-//   removeListeners: jest.fn(),
-//   isConnected: {
-//     fetch: () => {
-//       return Promise.resolve(true);
-//     },
-//     addEventListener: jest.fn(),
-//     removeEventListener: jest.fn(),
-//   },
-//   configure: jest.fn(),
-// };
